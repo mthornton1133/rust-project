ea8a72cef7810ea0145dcd86d7a3c0abea1cfa8a
mthornton1133 <matthew5@unc.edu>
2025-01-24 21:46:57 +0000

    Followed Rust Tutorial and can print a line

---
+++ hello-comp423/target/debug/deps/hello_comp423-cdf29d4d92d10a05.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/hello-comp423/target/debug/deps/libhello_comp423-cdf29d4d92d10a05.rmeta: src/main.rs
+
+/workspaces/rust-project/hello-comp423/target/debug/deps/hello_comp423-cdf29d4d92d10a05.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/deps/hello_comp423-f24ad8dd8d9e914b.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/hello-comp423/target/debug/deps/libhello_comp423-f24ad8dd8d9e914b.rmeta: src/main.rs
+
+/workspaces/rust-project/hello-comp423/target/debug/deps/hello_comp423-f24ad8dd8d9e914b.d: src/main.rs
+
+src/main.rs:
